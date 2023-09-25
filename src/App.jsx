19bf92548208da78cb7bc78911c04ec6f35eeb71
Veronica Rojas <ar_veronica@yahoo.com>
2023-09-25 15:41:50 +0100

    dice function

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,30 @@ import './App.css';
 import Die from './Die';
 
 function App() {
+
+  function allNewDice() {
+    const diceArray = [];
+    const min = Math.ceil(1);
+    const max = Math.floor(6);
+    while(diceArray.length < 10) {
+      const number = Math.floor(Math.random() * (max - min + 1) + min);
+      diceArray.push(number);
+    }
+    return diceArray;
+  }
+
+  
+
+
+
+
   return (
     <main>
-      <div className='die--container'>
-        
+
+      <h1 className='app--title'>Tenzies</h1>
+      <p className='app--description'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className='dice--container'>
+
       <Die value={1}/>
       <Die value={1}/>
       <Die value={1}/>
