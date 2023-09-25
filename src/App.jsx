25a591fb98b14738d89262bab2b13fb423e1bd80
Veronica Rojas <ar_veronica@yahoo.com>
2023-09-25 14:33:03 +0100

    body and main element

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,13 @@
-import { useState, useEffect } from 'react'
-import Confetti from 'react-confetti';
-import './App.css'
-import Die from './Die';
+import React from 'react'
+import './App.css';
 
-export default function App() {
-  const [dice, setDice] = useState(allNewDice())
-  const [tenzies, setTenzies] = useState(false)
-  
-  useEffect(() => {
-      const firstValue = dice[0].value
-      const allHeld = dice.every(die => die.held)
-      const allSameNumber = dice.every(die => die.value === firstValue)
-      if(allHeld && allSameNumber) {
-          setTenzies(true)
-      }
-  }, [dice])
-  
-  function randomDieValue() {
-      return Math.ceil(Math.random() * 6)
-  }
-
-  function allNewDice() {
-      const newArray = []
-      for(let i = 0; i < 10; i++) {
-          const newDie = {
-              value: randomDieValue(),
-              held: false,
-              id: i + 1
-          }
-          newArray.push(newDie)
-      }
-      return newArray
-  }
-
-  function rollUnheldDice() {
-      if (!tenzies) {
-          setDice((oldDice) => oldDice.map((die, i) =>
-              die.held ? 
-                  die : 
-                  { value: randomDieValue(), held: false, id: i + 1 }
-          ))
-      } else {
-          setDice(allNewDice())
-          setTenzies(false)
-      }
-  }
-
-  function holdDice(id) {
-      setDice(prevDice => prevDice.map(die => {
-          return die.id === id ? 
-              {...die, held: !die.held} : 
-              die
-      }))
-  }
-
-  const diceElements = dice.map((die) => (
-      <Die key={die.id} {...die} hold={() => holdDice(die.id)} />
-  ))
-
+function App() {
   return (
-      <main>
-          {tenzies && <Confetti />}
-          <h1>Tenzies</h1>
-          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-          <div className="die-container">{diceElements}</div>
-          <button className="roll-dice" onClick={rollUnheldDice}>
-              {tenzies ? "Reset Game" : "Roll"}
-          </button>
-      </main>
+    <main>
+      
+
+    </main>
   )
 }
+
+export default App
